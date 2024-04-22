18eb08355ad80f5567920cf03ac2bab150ccbcb6
Patrick_Andrada <patrickanthonio993@gmail.com>
2024-04-22 10:04:44 +0800

    Adding new user on db

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {MongoClient, ServerApiVersion} from "mongodb"
+import { MongoClient, ServerApiVersion } from "mongodb";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
@@ -33,26 +33,3 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
-
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(mongoURI, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   }
-// });
-// async function run() {
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//     await client.connect();
-//     // Send a ping to confirm a successful connection
-//     await client.db("admin").command({ ping: 1 });
-//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// }
-// run().catch(console.dir);
